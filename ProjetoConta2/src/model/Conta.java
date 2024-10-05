587fc9289b3266f3c578ad74ccde0e65f6bce6ad
Nefi Lehi <136665322+nefilehi@users.noreply.github.com>
2024-10-04 21:14:45 -0300

    projeto para gerenciar contas do banco

---
+++ ProjetoConta2/src/model/Conta.java
@@ -0,0 +1,62 @@
+package model;
+
+public abstract class Conta {
+    private int numero;
+    private String nomeCliente;
+    private double saldo;
+
+    public Conta(int numero, String nomeCliente) {
+        this.numero = numero;
+        this.nomeCliente = nomeCliente;
+        this.saldo = 0.0;
+    }
+
+    public boolean sacar(double valor) {
+        if(this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+    public boolean depositar(double valor) {
+        if(valor > 0){
+            this.saldo += valor;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Dados da conta: \n" +
+                "numero da conta" + numero + "\n" +
+                "Cliente: " + nomeCliente + "\n" +
+                "Saldo: " + saldo + "\n";
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    //metodo abstrato para cobrar tarifa (cada tipo de conta terá uma tarifa diferente
+    abstract void cobrarTarifa();
+}
