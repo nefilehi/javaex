05df0714554c2ec30d9248eb473960d3cbab2d96
Nefi Lehi <136665322+nefilehi@users.noreply.github.com>
2024-11-08 20:43:11 -0300

    aaa

---
+++ ExemploHash/src/Exemplo.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Exemplo {
+
+    public static void main(String[] args) {
+
+        List<Aluno> alunos = new ArrayList<>();
+
+        Aluno aluno1 = new Aluno(11, "Maria");
+        Aluno aluno2 = new Aluno(22, "Pedro");
+        Aluno aluno3 = new Aluno(33, "Antonio");
+
+        alunos.add(aluno1);
+        alunos.add(aluno2);
+        alunos.add(aluno3);
+
+        System.out.println("Tamanho da lista: " + alunos.size());
+
+        Scanner sc = new Scanner(System.in);
+        Aluno alunoProc = new Aluno();
+        System.out.println("Digite a matricula a ser encontrada: ");
+        alunoProc.setMatricula(Integer.parseInt(sc.nextLine()));
+        if(alunos.contains(alunoProc)) {
+            System.out.println("Aluno encontrado");
+            int indice = alunos.indexOf(alunoProc);
+            alunoProc = alunos.get(indice);
+            System.out.println("Nome do(a) aluno(a) " + alunoProc);
+        } else {
+            System.out.println("Aluno não encontrado");
+        }
+
+    }
+}
